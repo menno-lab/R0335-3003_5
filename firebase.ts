192857c7c4931047c912963a7cdc01c15491b003
parkharo <arkharovp@gmail.com>
2021-11-16 11:22:40 +0200

    initial commit, added firebase.ts setup file

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACDZVKEvMvi3kHU-V0UaTnJbkWlDKcmio",
+  authDomain: "todo-r0335.firebaseapp.com",
+  projectId: "todo-r0335",
+  storageBucket: "todo-r0335.appspot.com",
+  messagingSenderId: "540307946986",
+  appId: "1:540307946986:web:6d70cd8b7bf469242dc693"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
